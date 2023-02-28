ffbc2a1ab6801d0610af14166f2f0c360bee6722
Hashim Siddiqui <hashimedt@gmail.com>
2023-02-28 09:40:05 -0600

    Fetch is working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react"
+
+
+
 
 function App() {
+
+  let [imageUrls, setImageUrls] = useState(0)
+  let [loading, setLoading] = useState(false)
+  let [text, setText] = useState()
+
+   //useEffect(()=>{generate()}, [])
+
+
+  let array1 = ["A", "B", "C", "D"]
+  //let array2 = ["Hey", "Yo"]
+  //let array3 = ["WhatsUp", "Man"]
+  //let url = `https://apimeme.com/meme?meme=${array1[Math.floor(Math.random()*2)]}&top=${array2[Math.floor(Math.random()*2)]}&bottom=${array3[Math.floor(Math.random()*2)]}`
+
+
+  async function generate(){
+    setLoading(true)
+    let url = "https://api.imgflip.com/get_memes"
+    const response = await fetch(url)
+    let data = await response.json()
+    
+    console.log(data)
+
+    let Urls = data.data.memes.map((item)=> {
+      return <img alt = "abc"  key = {item.id} className= {array1[Math.random() * 3 ]} src= {item.url} /> 
+    })
+    console.log(Urls)
+
+
+    await new Promise(resolve => setTimeout(resolve, 1000))
+
+    setImageUrls(Urls)
+    setLoading(false)
+  }
+  
+  console.log(imageUrls)
+  
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Click on the button to <br/> generate a meme</h1>
+      <div><button onClick = {generate}> Generate Meme</button> </div>
+      <br/>
+      { loading ? 
+      <img src="https://cdn3.iconfinder.com/data/icons/glypho-generic-icons/64/cog-settings-512.png" className="App-logo" alt="logo" /> : <> </>}
+      <div className = "Loading">
+      </div>
+      <div style={{ position: "relative" }} >
+      {imageUrls[4]}
+      <div style={{ position: "absolute",  bottom: "8px", left: "16px", fontSize: "80px" }} > abc {text} </div>
+      </div>
     </div>
   );
 }
