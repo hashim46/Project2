7ca416de726bec7aa8413619553fe00b4c8d0ffc
Hashim Siddiqui <hashimedt@gmail.com>
2023-03-06 10:42:54 -0600

    updated readme

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,16 @@ function About(){
 
         <h1> About Page</h1>
 
-        <p> It is Simple </p>
+        <p>  •  It is Simple  </p>
+
+        <p> •  You choose a random image by a click of a button</p>
+
+        <p> •  Then the user input the top text for the meme and hit Set Header Button </p>
+
+        <p> •  Then the user input the bottom text for the meme and hit Set Footer Button </p>
+
+        <p> •  Then you have you customized meme  </p>
+
 
         </div>
     )
