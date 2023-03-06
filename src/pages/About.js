58e195a974048443bf9921cedb1c46c9e5cfc517
Hashim Siddiqui <hashimedt@gmail.com>
2023-03-06 14:08:06 -0600

    typo fixed

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,17 +3,17 @@ function About(){
         <div> 
 
 
-        <h1> Intructions:</h1>
+        <h1> Instructions:</h1>
 
-        <p>  •  Click the The App Tab  </p>
+        <p>    Click the The App Tab  </p>
 
-        <p> •  You choose a random image by a click of a button</p>
+        <p>   You choose a random image by a click of a button</p>
 
-        <p> •  Then the user input the top text for the meme and hit Set Header Button </p>
+        <p>   Then the user input the top text for the meme and hit Set Header Button </p>
 
-        <p> •  Then the user input the bottom text for the meme and hit Set Footer Button </p>
+        <p>   Then the user input the bottom text for the meme and hit Set Footer Button </p>
 
-        <p> •  Then you have you customized meme  </p>
+        <p>   Then you have you customized meme  </p>
 
 
         </div>
